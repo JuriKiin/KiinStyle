d4c63bf76b868a8ff2b553edff890c49770d2e33
jurikiin <kiin.juri@gmail.com>
2019-03-18 22:48:06 -0400

    - Updated Documentation Layout

---
+++ js/kiinStyle.js
@@ -0,0 +1,12 @@
+document.onload = init;
+
+//This init funciton is used to generate certian HTML elements used with the framework.
+function init() {
+    for(let i = 0; i < document.getElementsByClassName('sticky'); i++) {
+
+    }
+}
+
+function ksToggleHam(e) {
+    e.classList.toggle("change");
+}
