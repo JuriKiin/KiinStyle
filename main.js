089c41856d65f1b213cbe67a03458189e406bb76
jurikiin <kiin.juri@gmail.com>
2019-05-29 23:32:00 +0200

    - Added Search functionality to the documentation

---
+++ main.js
@@ -0,0 +1,29 @@
+let idElements = [];
+
+window.onload = init;
+
+function init() {
+    idElements = document.getElementsByClassName("ks-section");
+}
+
+document.getElementById("search").addEventListener("search", function() {
+    Search();
+})
+
+function Search() {
+    let value = document.getElementById("search").value;
+    if(value == "") {
+        for(var i = 0; i < idElements.length; i++) {
+            idElements[i].classList.remove("d-none");
+        }
+    };
+    for(var i = 0; i < idElements.length; i++) {
+        let name = idElements[i].id.toUpperCase();
+        if(name.includes(value.toUpperCase())) {
+            document.getElementsByClassName("ks-menuItem")[i].classList.remove("d-none");
+        } else {
+            document.getElementsByClassName("ks-menuItem")[i].classList.add("d-none");
+        }
+    }
+
+}
